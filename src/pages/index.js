835e205cc7e6a9dc696002463c522bf822eaad90
Aviv Eyal <aviveyal07@gmail.com>
2022-08-12 13:13:08 +0300

    desc

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,8 +22,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title="Cryptoverse Capital"}
+      description="Backing the Next Generation of Crypto Icons">
       <HomepageHeader className='white-bcg'/>
       <main>
       </main>
